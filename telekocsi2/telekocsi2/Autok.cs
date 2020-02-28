51c6e536b7a186ec515356a8d9c8fe0994f903c1
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-02-28 22:15:57 +0100

    Add files via upload

---
+++ telekocsi2/telekocsi2/Autok.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telekocsi2
+{
+    class Autok
+    {//Indulás;Cél;Rendszám;Telefonszám;Féröhely
+        public string Inulas;
+        public string Cel;
+        public string Rendszam;
+        public string Telefonszam;
+        public int Ferohely;
+        public string IndulCel;
+
+        public Autok (string sor)
+        {
+            var dbok = sor.Split(';');
+            this.Inulas = dbok[0];
+            this.Cel = dbok[1];
+            this.Rendszam = dbok[2];
+            this.Telefonszam = dbok[3];
+            this.Ferohely = int.Parse(dbok[4]);
+            this.IndulCel= dbok[0]+"-"+ dbok[1];
+
+
+        }
+
+    }
+}
---
+++ telekocsi2/telekocsi2/Igenyek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telekocsi2
+{
+    class Igenyek
+    {
+        //Azonosító;Indulás;Cél;Személyek
+        public string Azonosito;
+        public string Indulas;
+        public string Cel;
+        public int Szemelyek;
+        public string IndulCel;
+
+        public Igenyek (string sor)
+        {
+            var dbok = sor.Split(';');
+            this.Azonosito = dbok[0];
+            this.Indulas = dbok[1];
+            this.Cel = dbok[2];
+            this.Szemelyek = int.Parse(dbok[3]);
+            this.IndulCel = dbok[1] + "-" + dbok[2];
+
+        }
+
+    }
+}
